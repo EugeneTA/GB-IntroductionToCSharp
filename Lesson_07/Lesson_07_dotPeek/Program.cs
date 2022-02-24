eb5ea3ba2b7e78bbe93d99f88fbade4af6ff1653
EugeneTA <tishkov2005@yandex.ru>
2022-02-24 12:08:16 +0300

    Lesson_07

---
+++ Lesson_07/Lesson_07_dotPeek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson_07_dotPeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Lesson 7. This is the base project for the dotPeek");
+            Console.WriteLine();
+            Console.WriteLine(" Hit any key to exit ...");
+            Console.ReadKey();
+        }
+    }
+}
