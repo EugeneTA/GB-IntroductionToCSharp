1fdb07a64f8d81a8e7bb67fb082ae6ee5f2340c3
EugeneTA <tishkov2005@yandex.ru>
2022-02-27 15:11:24 +0300

    Lesson 08 homework

---
+++ Lesson_08/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Урок 8. Основные сущности проекта .Net
+            // Создать консольное приложение, которое при старте выводит приветствие, записанное в настройках приложения(application - scope).
+            // Запросить у пользователя имя, возраст и род деятельности, а затем сохранить данные в настройках.
+            // При следующем запуске отобразить эти сведения.
+            // Задать приложению версию и описание.
+
+
+            if (string.IsNullOrWhiteSpace(Properties.Settings.Default.Greetings) == false)
+            {
+                Console.WriteLine($" {Properties.Settings.Default.Greetings}");
+
+                // Если в настройках приложения не задано имя пользователя, то запрашиваем
+                // имя, возраст и род деятельности у пользователя.
+
+                if (string.IsNullOrEmpty(Properties.Settings.Default.UserName))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(" Как вас зовут?");
+                    Console.Write(" > ");
+                    Properties.Settings.Default.UserName = Console.ReadLine();
+
+                    int userAge = -1;
+                    
+                    while(userAge < 0)
+                    {
+                        try
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine(" Сколько вам полных лет?");
+                            Console.Write(" > ");
+                            userAge = Convert.ToUInt16(Console.ReadLine());
+
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine(" Возраст введен неверно. Используйте только числа для ввода возраста, например 33.");
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine(" Возраст введен неверно. Введенное число слишком большое или меньше 0.");
+                        }
+                    }
+
+                    Properties.Settings.Default.UserAge = Convert.ToUInt32(userAge);
+
+                    Console.WriteLine();
+                    Console.WriteLine(" Кем вы работаете?");
+                    Console.Write(" > ");
+                    Properties.Settings.Default.UserOccupation = Console.ReadLine();
+
+                    try
+                    {
+                        Properties.Settings.Default.Save();
+                        Console.WriteLine();
+                        Console.WriteLine(" Данные успешно сохранены в настройках.");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($" Не удалось сохранить настройки. Ошибка: {e.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($" Здравствуйте, {Properties.Settings.Default.UserName}!");
+                    Console.WriteLine($" Ваш возраст - {Properties.Settings.Default.UserAge} {GetAgeWord(Properties.Settings.Default.UserAge)}.");
+                    Console.WriteLine($" Ваша профессия -  {Properties.Settings.Default.UserOccupation}.");
+                }
+            }
+
+            Console.WriteLine();
+            Console.Write(" Нажмите любую клавишу для завершения работы ...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Возвращает слова "год", "года" или "лет" в зависимости от возраста.
+        /// Например для 11 вернет "лет", а для 1 - "год".
+        /// </summary>
+        /// <param name="age">возраст</param>
+        /// <returns></returns>
+        static public string GetAgeWord(uint age)
+        {
+            string result = "";
+
+            if (0 <= age)
+            {
+                if ((age%10 == 1) && (age != 11))
+                {
+                    result = "год";
+                }
+                else if (((age % 10 == 2) || (age % 10 == 3) || (age % 10 == 4)) && (age != 12) && (age != 13) && (age != 14))
+                {
+                    result = "года";
+                }
+                else
+                {
+                    result = "лет";
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ Lesson_08/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Урок 08")]
+[assembly: AssemblyDescription("Домашнее задание к уроку 8")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Введение в С#")]
+[assembly: AssemblyCopyright("Copyright © Tishkov.E.A. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c35331ff-8153-47cc-bcd4-99ca0bf5b544")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: NeutralResourcesLanguage("ru")]
